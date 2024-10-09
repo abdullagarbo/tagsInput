388a1a627e3f647895b16f9aca3317f297250e26
Abdulla <abdulla.garbo@gmail.com>
2024-10-09 13:11:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import IconTags from './components/UI/IconTags';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className='logo'>
+          <IconTags />
+        </p>
+      </div>
+      <h1>Tags Input</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/UI/IconTags.jsx
@@ -0,0 +1,32 @@
+function IconTags(props) {
+  return (
+    <svg
+      viewBox='0 0 21 21'
+      fill='currentColor'
+      height='10em'
+      width='10em'
+      {...props}
+    >
+      <g fill='none' fillRule='evenodd' transform='translate(1 3)'>
+        <path
+          stroke='currentColor'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M11.914.5H15.5a2 2 0 012 2v3.586a1 1 0 01-.293.707l-6.793 6.793a2 2 0 01-2.828 0l-3.172-3.172a2 2 0 010-2.828L11.207.793A1 1 0 0111.914.5z'
+        />
+        <path
+          stroke='currentColor'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M7.5 13.5l-2.013 1.006A2 2 0 012.72 13.42L1.105 9.114a2 2 0 01.901-2.45L9.5 2.5'
+        />
+        <path
+          fill='currentColor'
+          d='M15 2 H15 A1 1 0 0 1 16 3 V3 A1 1 0 0 1 15 4 H15 A1 1 0 0 1 14 3 V3 A1 1 0 0 1 15 2 z'
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default IconTags;
